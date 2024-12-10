5bb93c0720d73e08bd5042e9c48836692458aa82
azwansazali <azwansazali@gmail.com>
2024-12-10 16:50:12 +0800

    Test 1 Question 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# banking-application" 
+"# banking-application" 
--- banking_app.py
+++ banking_app.py
@@ -1,13 +1,16 @@
 class Account:
     def __init__(self, account_number, account_holder, balance=0):
+        # Initialize account with account number, holder, and balance
         self.account_number = account_number
         self.account_holder = account_holder
         self.balance = balance
 
     def deposit(self, amount):
+        # Increase balance deposit amount
         self.balance += amount
 
     def withdraw(self, amount):
+        # Decrease balance withdrawal amount if sufficient
         if amount <= self.balance:
             self.balance -= amount
         else:
@@ -16,12 +19,15 @@ class Account:
 
 class SavingAccount(Account):
     def __init__(self, account_number, account_holder, balance=0, interest_rate=0.01):
+        # Initialize SavingAccount with base Account properties and interest rate
         super().__init__(account_number, account_holder, balance)
         self.interest_rate = interest_rate
 
     def calculate_interest(self):
+        # Calculate interest based balance and interest rate
         return self.balance * self.interest_rate
 
+# Print results
 if __name__ == "__main__":
     savings = SavingAccount("55555", "Azwan", 1000)
 
